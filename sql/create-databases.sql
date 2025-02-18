c2287a795aa2811b18f10bc993d2da28daaef4c8
Emilio-115 <emivazcru@alum.us.es>
2025-02-18 12:13:35 +0100

    refactor: :wrench: Renamed pom project name and added D01 version

--- sql/create-databases.sql
+++ sql/create-databases.sql
@@ -1,19 +1,19 @@
-drop database if exists `Acme-ANS-D01-25.1.0`;
-create database `Acme-ANS-D01-25.1.0` 
+drop database if exists `Acme-ANS-D01`;
+create database `Acme-ANS-D01` 
 	character set = 'utf8mb4'
   	collate = 'utf8mb4_unicode_ci';
 
 grant select, insert, update, delete, create, drop, references, index, alter, 
         create temporary tables, lock tables, create view, create routine, 
         alter routine, execute, trigger, show view
-    on `Acme-ANS-D01-25.1.0`.* to 'acme-user'@'%';
+    on `Acme-ANS-D01`.* to 'acme-user'@'%';
 
-drop database if exists `Acme-ANS-D01-25.1.0-Test`;
-create database `Acme-ANS-D01-25.1.0-Test` 
+drop database if exists `Acme-ANS-D01-Test`;
+create database `Acme-ANS-D01-Test` 
 	character set = 'utf8mb4'
   	collate = 'utf8mb4_unicode_ci';
 
 grant select, insert, update, delete, create, drop, references, index, alter, 
         create temporary tables, lock tables, create view, create routine, 
         alter routine, execute, trigger, show view
-    on `Acme-ANS-D01-25.1.0-Test`.* to 'acme-user'@'%';
+    on `Acme-ANS-D01-Test`.* to 'acme-user'@'%';
