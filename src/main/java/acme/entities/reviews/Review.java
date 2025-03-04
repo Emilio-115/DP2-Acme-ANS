6a58a9875e5831e3c2dde41371452c0ee4ae2952
miniluz <javiermiladossantos@gmail.com>
2025-03-04 17:04:35 +0100

    feat: :sparkles: Create review entity

---
+++ src/main/java/acme/entities/reviews/Review.java
@@ -0,0 +1,57 @@
+
+package acme.entities.reviews;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Review {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(min = 1, max = 50)
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				moment;
+
+	@Mandatory
+	@ValidString(min = 1, max = 50)
+	@Automapped
+	private String				subject;
+
+	@Mandatory
+	@ValidString(min = 1, max = 50)
+	@Automapped
+	private String				body;
+
+	@Optional
+	@ValidNumber(min = 0, max = 10, integer = 2)
+	@Automapped
+	private Double				score;
+
+	@Optional
+	@Valid
+	@Automapped
+	private Boolean				recommended;
+
+}
