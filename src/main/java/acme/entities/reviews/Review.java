ad9a485bdfc4529a7ee48f2fe6d2698f12d28e20
miniluz <javiermiladossantos@gmail.com>
2025-03-04 17:09:55 +0100

    fix: :bug: Extend AbstractEntity

--- src/main/java/acme/entities/reviews/Review.java
+++ src/main/java/acme/entities/reviews/Review.java
@@ -8,6 +8,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
 
+import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
@@ -20,7 +21,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-public class Review {
+public class Review extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
 
