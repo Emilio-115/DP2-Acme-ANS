f1c2867862436e86fede6ff4ab54f23a2ce9472d
miniluz <javiermiladossantos@gmail.com>
2025-03-04 16:13:07 +0100

    build: :construction_worker: Update create-databases script

--- sql/create-databases.sql
+++ sql/create-databases.sql
@@ -1,19 +1,19 @@
-drop database if exists `Acme-ANS-D01`;
-create database `Acme-ANS-D01` 
+drop database if exists `Acme-ANS-D02`;
+create database `Acme-ANS-D02` 
+	character set = 'utf8mb4' 
+  	collate = 'utf8mb4_unicode_ci';
+
+grant select, insert, update, delete, create, drop, references, index, alter, 
+        create temporary tables, lock tables, create view, create routine, 
+        alter routine, execute, trigger, show view
+    on `Acme-ANS-D02`.* to 'acme-user'@'%';
+
+drop database if exists `Acme-ANS-D02-Test`;
+create database `Acme-ANS-D02-Test` 
 	character set = 'utf8mb4'
   	collate = 'utf8mb4_unicode_ci';
 
 grant select, insert, update, delete, create, drop, references, index, alter, 
         create temporary tables, lock tables, create view, create routine, 
         alter routine, execute, trigger, show view
-    on `Acme-ANS-D01`.* to 'acme-user'@'%';
-
-drop database if exists `Acme-ANS-D01-Test`;
-create database `Acme-ANS-D01-Test` 
-	character set = 'utf8mb4'
-  	collate = 'utf8mb4_unicode_ci';
-
-grant select, insert, update, delete, create, drop, references, index, alter, 
-        create temporary tables, lock tables, create view, create routine, 
-        alter routine, execute, trigger, show view
-    on `Acme-ANS-D01-Test`.* to 'acme-user'@'%';
+    on `Acme-ANS-D02-Test`.* to 'acme-user'@'%';
