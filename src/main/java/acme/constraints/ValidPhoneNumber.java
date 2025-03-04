bdbd0313da143744967679159bf6e4d9398df95b
Emilio-115 <emivazcru@alum.us.es>
2025-03-04 16:21:49 +0100

    fix: :bug: Removed NotBlank and added white space to pattern

--- src/main/java/acme/constraints/ValidPhoneNumber.java
+++ src/main/java/acme/constraints/ValidPhoneNumber.java
@@ -9,7 +9,6 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.ReportAsSingleViolation;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Target(ElementType.FIELD)
@@ -17,8 +16,7 @@ import javax.validation.constraints.Pattern;
 @Constraint(validatedBy = {})
 @ReportAsSingleViolation
 
-@NotBlank
-@Pattern(regexp = "^+?\\d{6,15}$")
+@Pattern(regexp = " ^+?\\d{6,15}$")
 public @interface ValidPhoneNumber {
 
 	String message() default "{acme.validation.phonenumber.message}";
