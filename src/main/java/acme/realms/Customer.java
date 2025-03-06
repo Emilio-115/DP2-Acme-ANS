9708f8c060a3cc37744c1c02730e9ac0dbead9ee
Emilio-115 <emivazcru@alum.us.es>
2025-03-06 19:22:38 +0100

    refactor: :recycle: Phone prop contraint changed to ValidPhoneNumber

--- src/main/java/acme/realms/Customer.java
+++ src/main/java/acme/realms/Customer.java
@@ -10,6 +10,7 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.constraints.ValidInitialsNumberIdentifier;
 import acme.constraints.ValidPhoneNumber;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,7 +23,7 @@ public class Customer extends AbstractRole {
 	private static final long	serialVersionUID	= 1L;
 
 	@Mandatory
-	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$") //Mover a roles y este pattern habra que actualizarlo
+	@ValidInitialsNumberIdentifier
 	@Column(unique = true)
 	private String				identifier;
 
