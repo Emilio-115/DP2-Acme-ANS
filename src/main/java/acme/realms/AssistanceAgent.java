c73b9f7a5325ad51fd69f5ffda304a98b5a97159
Miguel Garcia Vizcaino <miguelgvizcaino@gmail.com>
2025-03-07 17:44:11 +0100

    feat: :art: creating the role of assistance agent

---
+++ src/main/java/acme/realms/AssistanceAgent.java
@@ -0,0 +1,63 @@
+
+package acme.realms;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidString;
+import acme.entities.airlines.Airline;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AssistanceAgent extends AbstractRole {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
+	@Column(unique = true)
+	private String				employeeCode;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				spokenLanguages;
+
+	@Mandatory
+	@Automapped
+	private Airline				airline;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				firstDay;
+
+	@Optional
+	@ValidString(max = 255)
+	@Automapped
+	private String				bio;
+
+	@Optional
+	@ValidMoney
+	@Automapped
+	private Money				salary;
+
+	@Optional
+	@Automapped
+	private String				image;
+
+}
