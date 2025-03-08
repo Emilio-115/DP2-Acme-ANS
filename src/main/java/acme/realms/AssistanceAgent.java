9b0cb74f87663b7bc9e14edba5779d43cfa8338b
Miguel Garcia Vizcaino <miguelgvizcaino@gmail.com>
2025-03-08 12:18:11 +0100

    fix: :art: arreglo

--- src/main/java/acme/realms/AssistanceAgent.java
+++ src/main/java/acme/realms/AssistanceAgent.java
@@ -5,8 +5,10 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractRole;
 import acme.client.components.datatypes.Money;
@@ -16,6 +18,8 @@ import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
+import acme.constraints.ValidInitialsNumberIdentifier;
 import acme.entities.airlines.Airline;
 import lombok.Getter;
 import lombok.Setter;
@@ -28,26 +32,27 @@ public class AssistanceAgent extends AbstractRole {
 	private static final long	serialVersionUID	= 1L;
 
 	@Mandatory
-	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
+	@ValidInitialsNumberIdentifier
 	@Column(unique = true)
 	private String				employeeCode;
 
 	@Mandatory
-	@ValidString(max = 255)
+	@ValidString
 	@Automapped
 	private String				spokenLanguages;
 
 	@Mandatory
-	@Automapped
+	@Valid
+	@ManyToOne
 	private Airline				airline;
 
 	@Mandatory
-	@ValidMoment(past = true)
+	@ValidMoment
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date				firstDay;
 
 	@Optional
-	@ValidString(max = 255)
+	@ValidString
 	@Automapped
 	private String				bio;
 
@@ -57,6 +62,7 @@ public class AssistanceAgent extends AbstractRole {
 	private Money				salary;
 
 	@Optional
+	@ValidUrl
 	@Automapped
 	private String				image;
 
