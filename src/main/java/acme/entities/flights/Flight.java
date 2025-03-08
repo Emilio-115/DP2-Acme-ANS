1d34eb3f7840e8d5365df8215f58b1cde5f1732a
Pepe Coral <coritochico@gmail.com>
2025-03-08 18:04:58 +0100

    feat: :sparkles: added the transient methods for flights but not working yet

--- src/main/java/acme/entities/flights/Flight.java
+++ src/main/java/acme/entities/flights/Flight.java
@@ -1,8 +1,13 @@
 
-package acme.entities;
+package acme.entities.flights;
+
+import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Transient;
 import javax.validation.Valid;
+
+import org.apache.commons.lang3.NotImplementedException;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
@@ -53,5 +58,31 @@ public class Flight extends AbstractEntity {
 	@ValidString
 	@Automapped
 	private String				description;
+
+
+	@Transient
+	public Date scheduledDeparture() {
+		throw new NotImplementedException();
+	}
+
+	@Transient
+	public Date scheduledArrival() {
+		throw new NotImplementedException();
+	}
+
+	@Transient
+	public String origin() {
+		throw new NotImplementedException();
+	}
+
+	@Transient
+	public String destiny() {
+		throw new NotImplementedException();
+	}
+
+	@Transient
+	public int numberOfLayovers() {
+		throw new NotImplementedException();
+	}
 
 }
