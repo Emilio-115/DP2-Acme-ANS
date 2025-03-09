9e36b449996a2f5b064cf1808867c25214a559d3
Miguel Garcia Vizcaino <miguelgvizcaino@gmail.com>
2025-03-09 17:47:02 +0100

    fix: :zap: añadir OneToOne relacion con Claim y cambio de nombre de variable

--- src/main/java/acme/entities/trackingLogs/TrackingLog.java
+++ src/main/java/acme/entities/trackingLogs/TrackingLog.java
@@ -4,8 +4,10 @@ package acme.entities.trackingLogs;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
@@ -13,6 +15,7 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidScore;
 import acme.client.components.validation.ValidString;
+import acme.entities.claims.Claim;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -31,7 +34,7 @@ public class TrackingLog extends AbstractEntity {
 	@Mandatory
 	@ValidString(max = 50)
 	@Automapped
-	private String				actualStep;
+	private String				undergoingStep;
 
 	@Mandatory
 	@ValidScore
@@ -46,4 +49,9 @@ public class TrackingLog extends AbstractEntity {
 	@ValidString
 	@Automapped
 	private String				resolutionDescription;
+
+	@Mandatory
+	@Valid
+	@OneToOne
+	private Claim				claim;
 }
