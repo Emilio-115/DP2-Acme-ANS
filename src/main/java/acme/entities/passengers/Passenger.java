adbe3ce4787d05cd88ab418b852164c950a94bf3
Emilio-115 <emivazcru@alum.us.es>
2025-03-11 12:21:00 +0100

    fix: :bug: Add missing annotations

--- src/main/java/acme/entities/passengers/Passenger.java
+++ src/main/java/acme/entities/passengers/Passenger.java
@@ -3,6 +3,7 @@ package acme.entities.passengers;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -14,7 +15,12 @@ import acme.client.components.validation.ValidEmail;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
 import acme.constraints.ValidPassport;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Passenger extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
