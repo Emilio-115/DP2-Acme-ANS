3b3a11268f3bf9c97443080914ba1293e2d38ae4
Emilio-115 <emivazcru@alum.us.es>
2025-03-27 13:36:12 +0100

    feat: :sparkles: Start passanger list

---
+++ src/main/java/acme/features/customer/passanger/CustomerPassengerRepository.java
@@ -0,0 +1,15 @@
+
+package acme.features.customer.passanger;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.passengers.Passenger;
+
+public interface CustomerPassengerRepository extends AbstractRepository {
+
+	@Query("SELECT br.associatedPassenger FROM BookingRecord WHERE br.associatedBooking.id = :bookingId")
+	List<Passenger> findPassengersByBookingId(Integer bookingId);
+}
