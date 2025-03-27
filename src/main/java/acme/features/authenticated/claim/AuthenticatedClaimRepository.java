4fecd28fd799764e1c5fc35d7c68bc85bc086f06
Miguel Garcia Vizcaino <miguelgvizcaino@gmail.com>
2025-03-27 13:57:30 +0100

    fix: :art: arreglando mas nombres

--- src/main/java/acme/features/authenticated/claim/AuthenticatedClaimRepository.java
+++ src/main/java/acme/features/authenticated/claim/AuthenticatedClaimRepository.java
@@ -1,18 +1,18 @@
 
-package acme.features.authenticated.assistancesAgents;
+package acme.features.authenticated.claim;
 
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 
-import acme.client.components.principals.UserAccount;
 import acme.client.repositories.AbstractRepository;
 import acme.entities.claims.Claim;
+import acme.realms.AssistanceAgent;
 
 public interface AuthenticatedClaimRepository extends AbstractRepository {
 
-	@Query("select ua from UserAccount ua where ua.id = :id")
-	UserAccount findUserAccountById(int id);
+	@Query("select aa from AssistanceAgent aa where aa.userAccount = :id")
+	AssistanceAgent findAssistanceAgentById(int id);
 
 	@Query("select c from Claim c where assistanceAgent.id = :id")
 	List<Claim> findClaimsByAssistanceAgent(int id);
--- src/main/java/acme/features/authenticated/claim/AuthenticatedListClaimService.java
+++ src/main/java/acme/features/authenticated/claim/AuthenticatedListClaimService.java
@@ -1,5 +1,5 @@
 
-package acme.features.authenticated.assistancesAgents;
+package acme.features.authenticated.claim;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import acme.client.components.models.Dataset;
 import acme.client.components.principals.Authenticated;
-import acme.client.components.principals.UserAccount;
 import acme.client.services.AbstractGuiService;
 import acme.entities.claims.Claim;
 import acme.realms.AssistanceAgent;
@@ -32,15 +31,14 @@ public class AuthenticatedListClaimService extends AbstractGuiService<Authentica
 	public void load() {
 		AssistanceAgent assistanceAgent;
 		int userAccountId;
-		UserAccount userAccount;
 
 		List<Claim> completedClaims = new ArrayList<>();
 		int id;
 
 		userAccountId = super.getRequest().getPrincipal().getAccountId();
-		userAccount = this.repository.findUserAccountById(userAccountId);
+		assistanceAgent = this.repository.findAssistanceAgentById(userAccountId);
 
-		id = super.getRequest().getData("id", int.class);
+		id = assistanceAgent.getId();
 		completedClaims = this.repository.findClaimsByAssistanceAgent(id);
 
 		super.getBuffer().addData(completedClaims);
@@ -50,7 +48,7 @@ public class AuthenticatedListClaimService extends AbstractGuiService<Authentica
 	public void unbind(final Claim claimList) {
 		Dataset dataset;
 
-		dataset = super.unbindObject(claimList, "registrationMoment", "passengerEmail", "description", "type", "isAccepted", "assistanceAgent", "leg");
+		dataset = super.unbindObject(claimList, "registrationMoment", "passengerEmail", "description", "type", "isAccepted", "completed", "assistanceAgent", "leg");
 
 		super.getResponse().addData(dataset);
 	}
